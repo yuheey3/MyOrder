4800444da43afd0e268981058f513f6adb969448
Yuki Waka <yuheey3@yahoo.co.jp>
2021-03-26 20:46:29 -0400

    Initial Commit

---
+++ MyOrder_Yuki_A3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOrder_Yuki_A3
+//
+//  Created by Yuki Waka on 2021-03-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
